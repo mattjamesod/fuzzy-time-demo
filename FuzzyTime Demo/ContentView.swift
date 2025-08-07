8fcb18ef81adb277e19d029d29b4da3d98554cd1
Matthew O'Donnell <matthew.james.od@gmail.com>
2025-08-07 20:24:14 +0100

    date grammars!

--- FuzzyTime Demo/ContentView.swift
+++ FuzzyTime Demo/ContentView.swift
@@ -46,18 +46,56 @@ func matchDate(from string: String) -> Date? {
 
 class DateMatcher {
     let string: String
-    let regex = try! Regex("([0-1]?[0-9]|2[0-3]):([0-5][0-9])")
     
     init(_ string: String) {
         self.string = string
     }
     
     func parseDate() -> Date? {
+        for grammar in DateGrammar.allCases {
+            if let date = grammar.match(string: string){
+                return date
+            }
+        }
+        
+        return nil
+    }
+}
+
+enum DateGrammar: String, CaseIterable {
+    case hour = "(0?[0-9]|1[0-2])(am|pm)"
+    case twentyFourHour = "([0-1]?[0-9]|2[0-3]):([0-5][0-9])"
+    case twelveHour = "(0?[0-9]|1[0-2]):([0-5][0-9])(am|pm)"
+    
+    var regex: Regex<AnyRegexOutput> { try! Regex(rawValue) }
+    
+    func match(string: String) -> Date? {
         guard let match = string.wholeMatch(of: regex) else { return nil }
         
-        guard let hour = Int(string[match[1].range!]) else { return nil }
-        guard let minute = Int(string[match[2].range!]) else { return nil }
+        let matchedHour = fetchHour(string: string, match: match)
+        let matchedMinute = fetchMinute(string: string, match: match)
+        let matchedAmpm = fetchAmPm(string: string, match: match)
+        
+        guard let matchedHour else { return nil }
+        
+        let hour = matchedHour + (matchedAmpm == "pm" ? 12 : 0)
+        let minute = matchedMinute ?? 0
         
         return Calendar.current.date(from: .init(hour: hour, minute: minute))
+    }
+    
+    private func fetchHour(string: String, match: Regex<Regex<AnyRegexOutput>.RegexOutput>.Match) -> Int? {
+        Int(string[match[1].range!])
+    }
+    
+    private func fetchMinute(string: String, match: Regex<Regex<AnyRegexOutput>.RegexOutput>.Match) -> Int? {
+        guard [.twentyFourHour, .twelveHour].contains(self) else { return nil }
+        return Int(string[match[2].range!])
+    }
+    
+    private func fetchAmPm(string: String, match: Regex<Regex<AnyRegexOutput>.RegexOutput>.Match) -> String? {
+        guard [.hour, .twelveHour].contains(self) else { return nil }
+        let position = self == .hour ? 2 : 3
+        return String(string[match[position].range!])
     }
 }
